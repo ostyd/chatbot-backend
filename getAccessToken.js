32f4848c44a87a038a326a3e936c6229a86615b2
ostyd <super_bond32@hotmail.com>
2025-05-30 13:20:00 +0300

    Update getAccessToken.js

--- getAccessToken.js
+++ getAccessToken.js
@@ -1,19 +1,19 @@
 // getAccessToken.js
-
 const fetch = require('node-fetch');
+require('dotenv').config();
 
 async function getAccessToken() {
-  const res = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
+  const response = await fetch('https://test.api.amadeus.com/v1/security/oauth2/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams({
       grant_type: 'client_credentials',
       client_id: process.env.AMADEUS_API_KEY,
-      client_secret: process.env.AMADEUS_API_SECRET,
-    }),
+      client_secret: process.env.AMADEUS_API_SECRET
+    })
   });
 
-  const data = await res.json();
+  const data = await response.json();
   return data.access_token;
 }
 
